12d7b08ef8845a8f21272615149408b0812a218e
John Cartrett <jnqpblc@gmail.com>
2024-10-24 15:39:57 -0400

    Update GPO-Enum-Writeable-Stats-ADM.ps1

--- AD/GPO-Enum-Writeable-Stats-ADM.ps1
+++ AD/GPO-Enum-Writeable-Stats-ADM.ps1
@@ -3,6 +3,7 @@ $Exclusions = @("SYSTEM", "Administrators", "Domain Admins", "Enterprise Admins"
 $Forest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
 $DomainList = @($Forest.Domains)
 $Domains = $DomainList |foreach { $_.GetDirectoryEntry() }
+$AllForestGpoACLs = @()
 foreach ($Domain in $Domains) {
     $Filter = "(&(objectCategory=groupPolicyContainer)(displayname=*))"
     $Searcher = New-Object System.DirectoryServices.DirectorySearcher
@@ -11,17 +12,21 @@ foreach ($Domain in $Domains) {
     $Searcher.PageSize = 1000
     $Searcher.SearchScope = "Subtree"
     $listGPO = $Searcher.FindAll()
-    $AllGpoACLs = @()
+    $AllDomainGpoACLs = @()
     foreach ($GPO in $listGPO){
-        $ACL = ([ADSI]$GPO.path).ObjectSecurity.Access |? {$_.ActiveDirectoryRights -match "Write" -and $_.AccessControlType -eq "Allow" -and  $Exclusions -notcontains $_.IdentityReference.toString().split("\")[1] -and $_.IdentityReference -ne "CREATOR OWNER"}
+        $ACL = ([ADSI]$GPO.path).ObjectSecurity.Access |? {$_.ActiveDirectoryRights -match "Write" -and $_.AccessControlType -eq "Allow" -and $Exclusions -notcontains $_.IdentityReference.toString().split("\")[1] -and $_.IdentityReference -ne "CREATOR OWNER"}
         if ($ACL -ne $null){
             $GpoACL = New-Object psobject
+            $GpoACL |Add-Member Noteproperty "Domain" $Domain.distinguishedName
+            $GpoACL |Add-Member Noteproperty "DisplayName" $GPO.Properties.displayname
             $GpoACL |Add-Member Noteproperty "IdentityReference" $ACL.IdentityReference
-            $AllGpoACLs += $GpoACL
+            $AllDomainGpoACLs += $GpoACL
         }
     }
+    $AllForestGpoACLs += $AllDomainGpoACLs
     Write-Output "`n`n###`n###`t[+] Got GPOs for $($Domain.distinguishedName):`n###`n"
-    $AllGpoACLs |Select-Object -ExpandProperty IdentityReference |Group-Object -Property Value |
+    $AllDomainGpoACLs |Select-Object -ExpandProperty IdentityReference |Group-Object -Property Value |
     Select-Object Name, Count |Sort-Object -Property Count -Descending
-    #Get-ADUser {REPLACE-WITH-USER} -Properties AccountNotDelegated,adminCount,Description,DistinguishedName,DoesNotRequirePreAuth,Enabled,HomeDirectory,HomeDrive,HomePage,LastBadPasswordAttempt,LastLogonDate,MemberOf,Name,PasswordExpired,PasswordLastSet,PasswordNeverExpires,PasswordNotRequired,pwdLastSet,PrincipalsAllowedToDelegateToAccount,ScriptPath,ServicePrincipalNames,SmartcardLogonRequired,Title
+    #Get-ADUser REPLACEWITHUSER -Properties AccountNotDelegated,adminCount,Description,DistinguishedName,DoesNotRequirePreAuth,Enabled,HomeDirectory,HomeDrive,HomePage,LastBadPasswordAttempt,LastLogonDate,MemberOf,Name,PasswordExpired,PasswordLastSet,PasswordNeverExpires,PasswordNotRequired,pwdLastSet,PrincipalsAllowedToDelegateToAccount,ScriptPath,ServicePrincipalNames,SmartcardLogonRequired,Title
+    #$AllForestGpoACLs |Where-Object { $_.IdentityReference -like "*REPLACEWITHUSER*" } |Select-Object Domain, DisplayName
 }
